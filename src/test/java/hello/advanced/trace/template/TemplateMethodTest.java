8caf2f916d1fc97c500f2d873df586319dcc0e28
Dongwoongkim <rlaehddnd0422@naver.com>
2024-07-18 21:31:50 +0900

    feature : 템플릿 메소드 패턴

---
+++ src/test/java/hello/advanced/trace/template/TemplateMethodTest.java
@@ -0,0 +1,67 @@
+package hello.advanced.trace.template;
+
+import hello.advanced.trace.code.AbstractTemplate;
+import hello.advanced.trace.code.SubClassLogic1;
+import hello.advanced.trace.code.SubClassLogic2;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class TemplateMethodTest {
+
+    @Test
+    public void templateMethodV0() throws Exception {
+        logic1();
+        logic2();
+    }
+
+    @Test
+    public void templateMethodV1() throws Exception {
+        AbstractTemplate template1 = new SubClassLogic1();
+        template1.execute();
+
+        AbstractTemplate template2 = new SubClassLogic2();
+        template2.execute();
+    }
+
+    @Test
+    public void templateMethodV2() throws Exception {
+        AbstractTemplate template1 = new AbstractTemplate() {
+            @Override
+            protected void call() {
+                log.info("비즈니스 로직3 실행");
+            }
+        };
+
+        // anonymous class
+//        log.info("클래스 이름 = {}", template1.getClass());
+
+        template1.execute();
+    }
+
+    private void logic1() {
+        long startTime = System.currentTimeMillis();
+
+        // 비즈니스 로직 실행
+        log.info("비즈니스 로직1 실행");
+        // 비즈니스 로직 종료
+
+        long endTime = System.currentTimeMillis();
+        long resultTime = endTime - startTime;
+
+        log.info("resultTime={}", resultTime);
+    }
+
+    private void logic2() {
+        long startTime = System.currentTimeMillis();
+
+        // 비즈니스 로직 실행
+        log.info("비즈니스 로직2 실행");
+        // 비즈니스 로직 종료
+
+        long endTime = System.currentTimeMillis();
+        long resultTime = endTime - startTime;
+
+        log.info("resultTime={}", resultTime);
+    }
+}
